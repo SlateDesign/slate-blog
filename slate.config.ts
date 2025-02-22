4d303073519363b8e20f04024d15272be2a25f0c
Irfan Kurnia <hello@irfankurnia.com>
2025-02-22 17:15:19 +0700

    Update Site Title and Description
    Update Site Title and Description

--- slate.config.ts
+++ slate.config.ts
@@ -5,13 +5,13 @@ import { defineConfig } from './src/helpers/config-helper';
 
 export default defineConfig({
   lang: 'en-US',
-  site: 'https://slate-blog-demo.vercel.app',
+  site: 'https://etam-blog.vercel.app',
   avatar: '/avatar.png',
-  title: 'Slate Blog',
-  description: 'Pure thoughts, simple stories.',
+  title: 'Erratic Thoughts and More',
+  description: 'A Personal Journal of Irfan Kurnia.',
   lastModified: true,
   readTime: true,
   footer: {
-    copyright: '© 2025 Slate Design',
+    copyright: '© 2025 Irfan Kurnia',
   }
 });
